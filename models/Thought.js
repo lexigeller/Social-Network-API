39810272c919dca1ce683307af93c3ee1c26822d
Lexi Geller <leximoconner@yahoo.com>
2023-09-17 11:23:30 -0400

    added Thought.js code

---
+++ models/Thought.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAt) => new Date(createdAt).toISOString()
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [ReactionSchema]
+});
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
